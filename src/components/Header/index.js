c71e338fae27aa995e3bab0ef577988fe6592fc4
Leandro Silva <lleandro.adal@gmail.com>
2026-05-24 16:38:31 -0300

    feat: add pages for Aluno, Alunos, Fotos, Login, and Register; update routes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,24 +3,20 @@ import { FaHome, FaSignInAlt, FaUser } from 'react-icons/fa';
 
 import { Nav } from './styled';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
-
   return (
     <>
       <Nav>
         <Link to="/">
           <FaHome size={24} />
         </Link>
-        <Link to="/login">
+        <Link to="/register">
           <FaUser size={24} />
         </Link>
-        <Link to="/sign">
+        <Link to="/login">
           <FaSignInAlt size={24} />
         </Link>
-        {botaoClicado ? 'Clicado' : 'Não clicado'}
       </Nav>
     </>
   );
