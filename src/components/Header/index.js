fda88e2a6b8226f78edf1f207ca03d4f5511ce41
Leandro Silva <lleandro.adal@gmail.com>
2026-05-25 16:49:11 -0300

    feat: refactor Header component and implement logout functionality; enhance Alunos page with delete confirmation and error handling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUser } from 'react-icons/fa';
+import {
+  FaHome,
+  FaSignInAlt,
+  FaUserAlt,
+  FaCircle,
+  FaPowerOff,
+} from 'react-icons/fa';
 
+import { Link, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { Nav } from './styled';
-import { Link } from 'react-router-dom';
+
+import * as actions from '../../store/modules/auth/actions';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const history = useHistory();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+  };
+
   return (
-    <>
-      <Nav>
-        <Link to="/">
-          <FaHome size={24} />
+    <Nav>
+      <Link to="/">
+        <FaHome size={24} />
+      </Link>
+      <Link to="/register">
+        <FaUserAlt size={24} />
+      </Link>
+
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          <FaPowerOff size={24} />
         </Link>
-        <Link to="/register">
-          <FaUser size={24} />
-        </Link>
+      ) : (
         <Link to="/login">
           <FaSignInAlt size={24} />
         </Link>
-      </Nav>
-    </>
+      )}
+
+      {isLoggedIn && <FaCircle size={24} color="#66ff33" />}
+    </Nav>
   );
 }
